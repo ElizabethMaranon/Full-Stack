274d3ff445e77984f91c492c7b1203a74ea1ec6c
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-16 15:24:23 +0100

    Eli

---
+++ CheckPoint/CheckPoint3/Ejercicio_6.py
@@ -0,0 +1,4 @@
+#Ejercicio 6: Utilice dividir para transformar la siguiente cadena en una lista.
+nombres = 'harry,alex,susie,jared,gail,conner'
+lista_nombres = nombres.split(',')
+print(lista_nombres)
---
+++ CheckPoint/CheckPoint3/Ejercicio_7.py
@@ -0,0 +1,8 @@
+#Ejercicio 7: Obtenga la primera palabra de su cadena usando índices.
+#Utilice la función superior para transformar las letras en mayúsculas.
+#Cree una nueva cadena que tome la palabra en mayúscula y el resto de la cadena original.
+frase = 'hades es mi perrito'
+print(frase[0:5])
+print(frase.upper())
+Frase = frase.capitalize()
+print(Frase)
