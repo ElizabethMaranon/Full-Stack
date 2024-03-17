893b5fa4993a624365074ea5ab4cd50e66e6fbc1
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-17 19:29:06 +0100

    Eli

--- Python/Variables.py
+++ Python/Variables.py
@@ -71,39 +71,42 @@ print(diccionario)
 print('Comillas dobles o simples indistintamente, pero la misma al principio y al final')
 sentence = 'the quick brown fox jumped over the lazy dog'
 comillas = "the quick brown fox jumped over the lazy dog"
-apostrofe_comillas = "cuando uses comillas dobles, apostofre don´t"
+apostrofe_comillas = "cuando uses comillas dobles, apostofre don't"
 print(apostrofe_comillas)
-apostrofe_comilla = 'cuando uses comillas simples, apostofre don\´t'
+apostrofe_comilla = 'cuando uses comillas simples, apostofre don\'t'
 print(apostrofe_comilla)
 barra_invertida = "barra invertida anula la comilla simple o doble que se use dentro de la string"
 print(barra_invertida)
 
 sentence = 'the quick brown fox jumped over the lazy dog'.upper()
+print("sentence = 'the quick brown fox jumped over the lazy dog.upper'()")
 print(sentence)
 print(sentence.upper()) 
+print("sentence_dos = sentence.upper()")
 sentence_dos = sentence.upper()
 print(sentence_dos)
 
-print('''.upper() mayuscula frase entera
-.capitalize() primera letra en mayuscula
-.title() primera letra de cada palabra en mayuscula
-.lower() minuscula frase entera
-''')
-
+print('variable.lower() minuscula frase entera')
+print(sentence.lower())
 sentence = 'the Quick brown fox juMped over the lazy dog'
 print(sentence)
+print('variable.upper() mayuscula frase entera')
 print(sentence.upper())
+print('variable.capitalize() primera letra en mayuscula')
 print(sentence.capitalize())
+print('variable.title() primera letra de cada palabra en mayuscula')
 print(sentence.title())
+print('variable.lower() minuscula frase entera')
 print(sentence.lower())
 
 print('strings contar el primer caracter seria 0, el segundo 1, el tercero 2, etc')
 
 sentence = 'The quick brown fox jumped over the lazy dog'
-print('sentence[4] solo imprime el caracter esoecificado entre los corchetes')
+print('sentence[4] solo imprime el caracter especificado entre los corchetes')
 print(sentence[4])
 
 print('first_word = sentence[0:3] #rango cuenta hasta justo antes del caracter numerado')
+print("new_sentence = 'Thy' + sentence[3:] #Rango a mantener, sin numero hasta es final")
 new_sentence = 'Thy' + sentence[3:] #Rango a mantener, sin numero hasta es final
 print(new_sentence)
 
