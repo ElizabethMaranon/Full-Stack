1e018bc31da783bb6dc323d0b7adfd58e3929afc
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-19 19:54:46 +0100

    Eli

--- Python/Number/08_Math_Functions.py
+++ Python/Number/08_Math_Functions.py
@@ -1,5 +1,73 @@
 import math
 print('')
-print('import math')
+print('import math biblioteca para operaciones')
+numeros ='''
+negativo = -20.25
+positivo = 89.99'''
+print(numeros)
 print('''
-''')
+-----''')
+negativo = -20.25
+positivo = 89.99
+valor_absoluto = '''
+abs -> valor absoluto elimina el negativo
+print(abs(negativo))'''
+print(valor_absoluto)
+print(abs(negativo))
+print('''
+-----''')
+redondeo_inferior = '''
+math.floor -> redondea hacia abajo
+print(math.floor(positivo))
+print(math.floor(negativo))'''
+print(redondeo_inferior)
+print(math.floor(positivo))
+print(math.floor(negativo))
+print('''
+-----''')
+redondeo_superior = '''
+math.ceil -> redondea hacia arriba
+print(math.ceil(positivo))
+print(math.ceil(negativo))'''
+print(redondeo_superior)
+print(math.ceil(positivo))
+print(math.ceil(negativo))
+print('''
+-----''')
+combinaciones_math = '''
+combina diferentes operaciones
+print(abs(math.ceil(positivo)))
+print(abs(math.ceil(negativo)))'''
+print(combinaciones_math)
+print(abs(math.floor(positivo)))
+print(abs(math.floor(negativo)))
+print('''
+-----''')
+redondear = '''
+round -> redondea al numero más cercano
+print(round(positivo))
+print(round(negativo))'''
+print(redondear)
+print(round(positivo))
+print(round(negativo))
+print('''
+-----''')
+raiz_cuadrada = '''
+math.sqrt -> raiz cuaddrada
+negartivo no tiene raiz cuadrada
+print(math.sqrt(positivo))'''
+print(raiz_cuadrada)
+print(math.sqrt(positivo))
+print('''
+-----''')
+exponente = '''
+math.pow -> exponencial
+print(math.pow(5, 2)) resultado float
+print(5**2) resultado entero
+print(math.pow(-5, 2))resultado float'''
+print(exponente)
+print(math.pow(5, 2))
+print(5**2)
+print(math.pow(-5, 2))
+print('''
+-----''')
