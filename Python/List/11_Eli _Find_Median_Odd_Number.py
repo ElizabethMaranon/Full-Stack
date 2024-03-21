1c04b4ceb5b763d9c6beb3f37157c5b99c5cb2a4
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-21 22:16:25 +0100

    Eli

--- Python/List/11_Eli _Find_Median_Odd_Number.py
+++ Python/List/11_Eli _Find_Median_Odd_Number.py
@@ -5,16 +5,18 @@ Tools:
 - sorted function
 - list slicing
 - computations
-lista = [6, 0, 5, 2, 1, 4, 3, 8, 7, 10, 9]
+lista = [600, 500, 200, 100, 400, 300, 800, 700, 900]
 -----''')
-lista = [6, 0, 5, 2, 1, 4, 3, 8, 7, 10, 9]
+lista = [600, 500, 200, 100, 400, 300, 800, 700, 900]
 '''print('print(len(lista))')
 print(len(lista))
+print('sorted_lista = sorted(lista)')
 sorted_lista = sorted(lista)
 print(sorted_lista)
 print('print(math.ceil(len(sorted_lista)/2))')
 print(math.ceil(len(sorted(lista))/2))
+print('media = sorted_lista[math.ceil(len(sorted(lista))/2)]')
 media = sorted_lista[math.ceil(len(sorted(lista))/2)]
 print(media)'''
 print('''-----''')
-print(math.ceil(len(sorted(lista))/2))
+print(sorted(lista)[math.ceil(len(sorted(lista))/2)])
