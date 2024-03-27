febfc3370da3cc784aa79f0e906c6d64e8712bc9
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-27 19:02:13 +0100

    Eli

--- Python/Dictionaries/07_Lists_of_Nested_Dictionaries.py
+++ Python/Dictionaries/07_Lists_of_Nested_Dictionaries.py
@@ -14,16 +14,17 @@ diccionario = [
     }
   }
 ]
-print('''
+diccionario_primero = diccionario[0].get('primero', 'Diccionario no encontrado')
+print(f'''
 Diccionarios dentro de un lista
 Importante apertura y cierre de corchetes
 Usar coma para separar valores, diccionarios, ....
 -----
 diccionario = [
-  {'primero': {'uno' : 'bat', 'dos' : 'bi', 'tres' : 'tres'}},
-  {'segundo' : {'diez' : 'hamar', 'veinte' : 'hogei', 'treinta' : 'hogeitahamar'}}
+  {{'primero': {{'uno' : 'bat', 'dos' : 'bi', 'tres' : 'tres'}}}},
+  {{'segundo' : {{'diez' : 'hamar', 'veinte' : 'hogei', 'treinta' : 'hogeitahamar'}}}}
 ]
+{diccionario}
 -----
-{df}
-
+{diccionario_primero}
 ''')
