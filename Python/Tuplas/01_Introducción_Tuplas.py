5e8770abb96e1db9507524a28d8b6c775aa9e8fa
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-29 19:10:04 +0100

    Eli

---
+++ Python/Tuplas/01_Introducción_Tuplas.py
@@ -0,0 +1,12 @@
+Zenbakiak = ('Bat', 'Bi', 'Hiru')
+Uno, Dos, Tres = Zenbakiak
+print(f"""
+Desempaquetar, como index en listas, pero en una sola línea.
+Inmutable. No se puede cambiar, ya que perdería la correlación de datos
+Zenbakiak = ('Bat', 'Bi', 'Hiru')
+Uno, Dos, Tres = Zenbakiak
+{{Uno}} -> {Uno}
+{{Dos}} -> {Dos}
+{{Tres}} -> {Tres}
+
+""")
---
+++ Python/Tuplas/02_Agregar_Elementos_Tupla.py
@@ -0,0 +1,11 @@
+zenbakiak = ('bat', 'bi', 'hiru')
+zenbakiak = zenbakiak + ('lau',)
+
+print(f"""
+Agregar reasignando,
+zenbakiak = ('bat', 'bi', 'hiru')
+Si no se pone la coma, Python lo tratará como operación matemática
+zenbakiak = zenbakiak + ('lau',)
+
+
+""")
