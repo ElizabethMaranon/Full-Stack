aa518d0e990b915503cf3f737c83cffdb7b0f68f
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-29 19:42:49 +0100

    Eli

--- Python/Tuplas/02_Agregar_Elementos_Tupla.py
+++ Python/Tuplas/02_Agregar_Elementos_Tupla.py
@@ -1,11 +1,31 @@
-zenbakiak = ('bat', 'bi', 'hiru')
-zenbakiak = zenbakiak + ('lau',)
-
+Zenbakiak = ('Bat', 'Bi', 'Hiru')
 print(f"""
 Agregar reasignando,
-zenbakiak = ('bat', 'bi', 'hiru')
-Si no se pone la coma, Python lo tratará como operación matemática
-zenbakiak = zenbakiak + ('lau',)
+Zenbakiak = ('Bat', 'Bi', 'Hiru') -> {Zenbakiak}
+crear una identificación id(zenbakiak) -> {id(Zenbakiak)}
+
+Añadir ',' para que no sea string, sino tupla
+Creamos una nueva tupla, anulando el nombre de la variable, dos maneras""")
+Zenbakiak = Zenbakiak + ('Lau',)
+print(f"""1º - Zenbakiak = Zenbakiak + ('Lau',) -> {Zenbakiak}
+crear una identificación id(zenbakiak) -> {id(Zenbakiak)} """) 
+Zenbakiak += ('Bost',)
+print(f"""2º - Zenbakiak += ('Bost') -> {Zenbakiak}
+crear una identificación id(zenbakiak) -> {id(Zenbakiak)} 
+
+Desempaquetamos
+Uno, Dos, Tres, Cuatro, Cinco = Zenbakiak""")
+Uno, Dos, Tres, Cuatro, Cinco = Zenbakiak
+print(f"""
+{{Uno}} -> {Uno}
+{{Dos}} -> {Dos}
+{{Tres}} -> {Tres}
+{{Cuatro}} -> {Cuatro}
+{{Cinco}} -> {Cinco}
+-----
+""")
+print(f"""
+
 
 
 """)
