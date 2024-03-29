462d93aec75b27e3af21262fde5aef136b138044
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-03-29 18:34:27 +0100

    Eli

--- Python/Listas/01_Introducción_Listas.py
+++ Python/Listas/01_Introducción_Listas.py
@@ -1,31 +1,25 @@
-print('''
+lista = ['Nombre, html, texto...']
+diccionario = {'Nombre': 'eli', 'html':'love', 'texto':'music'}
+tupla = ('Nombre, html, texto...')
+print(f'''
 herramienta mas crucial y fundamentales    
-list - > listas
-similar a una matriz, descriptiva
-lista de artículos, cualquier tipo datos
+[] list - > listas 
+similar a una matriz, descriptiva, cualquier tipo datos
 lista = ['Nombre, html, texto...']
-print(type(lista))
-print(lista)''')
-lista = ['Nombre, html, texto...']
-print(type(lista))
-print(lista)
-print('''-----''')
-print('''dictionary -> diccionario
+print(type(lista)) -> {type(lista)}
+print(lista) -> {lista}
+-----
+{{}}dictionary -> diccionario
 colección como la lista
 conjunto de claves con valores asociados
 diccionario = {'Nombre': 'eli', 'html':'love', 'texto':'music'}
-print(type(diccionario))
-print(diccionario)''')
-diccionario = {'Nombre': 'eli', 'html':'love', 'texto':'music'}
-print(type(diccionario))
-print(diccionario)
-print('''-----''')
-print('''tupla -> tupla
+print(type(diccionario)) -> {type(diccionario)}
+print(diccionario) -> {diccionario}
+-----
+()tupla -> tupla
 colección como la lista
 tupla = ('Nombre, html, texto...')
-print(type(tupla))
-print(tupla)''')
-tupla = ('Nombre, html, texto...')
-print(type(tupla))
-print(tupla)
-print('''-----''')
+print(type(tupla)) -> {type(tupla)}
+print(tupla) -> {tupla}
+-----
+''')
