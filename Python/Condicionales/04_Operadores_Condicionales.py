7d9f77036615e7e1aced33e08a7616c345392817
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-06 00:19:32 +0200

    Eli

---
+++ Python/Condicionales/04_Operadores_Condicionales.py
@@ -0,0 +1,13 @@
+print(f""" Operadores de comparación,
+lo que colocamos dentro de nuestra condición
+ == Igual (string, numbers, listas)
+ != No igual (string, numbers, listas)
+ <> No igual (No usar, anticuado)
+ >  Mayor (numbers)
+ >= Mayor o igual (numbers)
+ <  Menor (numbers)
+ <= menor o igual (numbers)
+      
+operador == 'igual'
+  print('==')""")
+
