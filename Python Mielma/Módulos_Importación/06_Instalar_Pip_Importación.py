8f9c531ff5cbf5f63b977eaa0b0a6e1c4719eae0
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-13 16:08:36 +0200

    Mielma

---
+++ Python Mielma/Módulos_Importación/06_Instalar_Pip_Importación.py
@@ -0,0 +1,10 @@
+"""Usar modulos creados por otros desarrolladores
+Ingresar paquedes  tienda PyPI o CheeseShoop
+Nos permite ingresar todos los modulos creados por otros desarrolladores
+
+https://bootstrap.pypa.io/get-pip.py
+descargar y ejecutar en el terminal -> python get-pip.py
+
+verificar si está instalada o saber version en la que está
+pip --version
+"""
--- Python Mielma/Módulos_Importación/main.py
+++
@@ -1,5 +0,0 @@
-import sys # importamos sistema
-sys.path.insert(0, './Libreria/') # importamos directorio
-import Otro_Directorio
-
-print(agurra('e', 'm'))
