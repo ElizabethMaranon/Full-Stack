b2f87b0a251cc914fa367e1dcb12bad12e52b661
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-21 18:19:16 +0200

    Mielma

--- Python/Módulos_Importación/08_Solucitud_Comunicación_Api.py
+++ Python/Módulos_Importación/08_Solucitud_Comunicación_Api.py
@@ -1,11 +1,14 @@
 """
+API -> application programming interface (Interfaz de Programación de Aplicaciones)
+Lenguaje que usa nuestra aplicación para comunicarse con otras aplicaciones
 Instalar -> Abrir terminal -> pip install requests
 pprint deberia de estar en el sistema, sino instalar mediante pip
 """
+
 import requests
 import pprint
-#r = requests.get('https://api.dailysmarty.com/posts')
-r = requests.get('https://www.behance.net/assets?tracking_source=nav20')
+
+r = requests.get('https://api.dailysmarty.com/posts')
 r.json()
-pprint.pprint(r.json()['posts'][0]) #paquete.funcion(var.json['diccionario key'][index, desde ultimo post])
+pprint.pprint(r.json()['posts'][0]) # paquete.funcion(var.json['diccionario key'][index, desde ultimo post])
 pprint.pprint(r.json()['posts'][0]['url_for_post']) #[Para obtener la url]
