89cdb57b753f7926fa72473eefb273d1c4396b2a
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-21 17:03:00 +0200

    Mielma

--- Python/Api_Comunicaciones/01_Solucitud_Comunicación_Api.py
+++ Python/Api_Comunicaciones/01_Solucitud_Comunicación_Api.py
@@ -4,7 +4,8 @@ pprint deberia de estar en el sistema, sino instalar mediante pip
 """
 import requests
 import pprint
-r = requests.get('https://api.dailysmarty.com/posts')
+#r = requests.get('https://api.dailysmarty.com/posts')
+r = requests.get('https://www.behance.net/assets?tracking_source=nav20')
 r.json()
 pprint.pprint(r.json()['posts'][0]) #paquete.funcion(var.json['diccionario key'][index, desde ultimo post])
 pprint.pprint(r.json()['posts'][0]['url_for_post']) #[Para obtener la url]
--- Python/Proyecto/Web Scraper/01_Construir_Web_Scraper.py
+++ Python/Proyecto/Web Scraper/01_Construir_Web_Scraper.py
@@ -9,7 +9,7 @@ hacer funcion que coga post y convertirlo en pagina"""
 """Pistas
 Librería request
 Librería inflection (averiguar que es)
-Librería beautifulsoup(para scrapinkg)
+Librería beautifulsoup (para scrapinkg)
 pip install request
 pip install inflection
 pip install beautifulsoup4 # hay que poner la ultima version """
