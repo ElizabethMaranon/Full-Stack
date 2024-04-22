e700bd5b47d5cc4d0f8898138a4916077ff7a623
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-22 19:21:43 +0200

    Mielma

---
+++ CheckPoint/CheckPoint 5/Iterar rangos.py
@@ -0,0 +1,6 @@
+"""
+Iterar rangos
+for num in range(desde, hasta no incluido, intervalo opcional) ->
+Desde el 1 hasta el 10(no incluido) con un intervalo de 2"""
+for num in range(1, 10, 2):
+  print(num)
