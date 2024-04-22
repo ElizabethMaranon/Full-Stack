37d9842a254ff01ffae231c9b64133283f485a9c
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-22 10:09:14 +0200

    Mielma

--- Python/Proyecto/Web Scraper/01_Construir_Web_Scraper.py
+++ Python/Proyecto/Web Scraper/01_Construir_Web_Scraper.py
@@ -13,3 +13,19 @@ Librería beautifulsoup (para scrapinkg)
 pip install request
 pip install inflection
 pip install beautifulsoup4 # hay que poner la ultima version """
+
+#https://es.kiosko.net/es/free.html
+import requests
+from bs4 import BeautifulSoup
+from inflection import titleize
+
+r = requests.get('https://es.kiosko.net/es/free.html')
+#print(r.text)#para imprimir la pagina en formato texto
+
+soup = BeautifulSoup(r.text, 'html.parser')
+links = soup.find_all('a')#buscará todos los enlaces
+#print(links)
+
+for link in links:
+    print(link['href']) 
+print(link['href'])
