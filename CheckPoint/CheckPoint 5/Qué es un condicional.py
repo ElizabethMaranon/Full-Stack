07e9cd18571a0b58f6a16a945da0a7b8fbfca761
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-22 19:16:32 +0200

    Mielma

--- CheckPoint/CheckPoint 5/Qué es un condicional.py
+++ CheckPoint/CheckPoint 5/Qué es un condicional.py
@@ -1,3 +1,8 @@
+"""Usuario y password para entrar en una página.
+if -> Si ambos son correctos (afirmativo) se le permite la entrada,
+elif -> Si ninguno de ellos es correcto (afirmativo), denegar acceso y ofrecer registrarse
+else -> En caso contrario (negativo),  Alguno de los datos no es el correcto
+"""
 nombre = 'Mielma'
 contraseña = 'secreto'
 
