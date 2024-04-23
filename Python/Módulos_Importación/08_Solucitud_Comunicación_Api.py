2095ece153701eca54ec238eea168c573956d912
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-23 15:28:42 +0200

    Mielma

--- Python/Módulos_Importación/08_Solucitud_Comunicación_Api.py
+++ Python/Módulos_Importación/08_Solucitud_Comunicación_Api.py
@@ -8,8 +8,8 @@ pprint deberia de estar en el sistema, sino instalar mediante pip install pprint
 import requests
 import pprint
 
-r = requests.get('https://api.dailysmarty.com/posts')
+r = requests.get('https://pokeapi.co/api/v2/pokemon/ditto')
 
 r.json()
-pprint.pprint(r.json()['posts'][0]) # paquete.funcion(var.json['diccionario key'][index, desde ultimo post])
-pprint.pprint(r.json()['posts'][0]['url_for_post']) #[Para obtener la url]
+pprint.pprint(r.json()['abilities'][1]) # paquete.funcion(var.json['diccionario key'][index, desde ultimo post])
+pprint.pprint(r.json()['abilities'][1]['ability']['url']) #[Para obtener la url]
