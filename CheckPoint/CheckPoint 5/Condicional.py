115ab43078406c444b5e32efbf5180b0dfb6a82a
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-23 17:27:19 +0200

    Mielma

--- CheckPoint/CheckPoint 5/Condicional.py
+++ CheckPoint/CheckPoint 5/Condicional.py
@@ -1,8 +1,4 @@
-"""Usuario y password para entrar en una página.
-if -> Si ambos son correctos (afirmativo) se le permite la entrada,
-elif -> Si ninguno de ellos es correcto (afirmativo), denegar acceso y ofrecer registrarse
-else -> En caso contrario (negativo),  Alguno de los datos no es el correcto
-"""
+
 nombre = 'Mielma'
 contraseña = 'secreto'
 
@@ -12,4 +8,3 @@ elif nombre != 'Mielma' and contraseña != 'Secreto':
   print('Sólo para usuarios registrados, ¿Desearías registrarte?')
 else:
   print('Alguno de los datos no es el correcto')
-
--- CheckPoint/CheckPoint 5/Iterar rangos.py
+++ CheckPoint/CheckPoint 5/Iterar rangos.py
@@ -4,3 +4,5 @@ for num in range(desde, hasta no incluido, intervalo opcional) ->
 Desde el 1 hasta el 10(no incluido) con un intervalo de 2"""
 for num in range(1, 10, 2):
   print(num)
+
+  
