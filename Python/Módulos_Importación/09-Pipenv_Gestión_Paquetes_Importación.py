b03d21c97d4b4586b09b8768d85ac0b11b648b11
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-04-24 10:52:36 +0200

    Mielma

---
+++ Python/Módulos_Importación/09-Pipenv_Gestión_Paquetes_Importación.py
@@ -0,0 +1,6 @@
+"""
+pipenv es un gestor de bibliotecas
+Cuando hacemos un proyecto, importamos los pawquetes que necesitemos, con la versión que está en ese momento.
+Al hacer otro proyecto, importamos la misma biblioteca pero con una versión más nueva
+Al instalar la versión nueva, se sobreescribe en la anterior anulando las versiones viejas y rompiendo las aplicaciones anteriores.
+En esos casos está pipenv, que gestiona y guarda ls bibliotecas usadas de manera virtual, para que cuando se actualizen las nuevas, las anteriores queden intactas"""
