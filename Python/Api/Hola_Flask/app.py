1ae34abc4957f618ee3c3017d97221d160671790
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-05-11 22:48:30 +0200

    Mielma

--- Python/Api/Hola_Flask/app.py
+++ Python/Api/Hola_Flask/app.py
@@ -1,13 +1,34 @@
-# Agregamos las dependencias instaladas, Diferencia mayúsculas de minúsculas
-from flask import Flask 
+from flask import Flask # Agregamos las dependencias instaladas, Diferencia mayúsculas de minúsculas
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
+import os # Librería del sistema operativo
 
 app = Flask(__name__) # Creamos variable, en este caso la más básica
 
-directorio_base = os.path.abspath(os.path.dirname(__file__))# Convertir una ruta relativa en una ruta absoluta
+directorio_base = os.path.abspath(os.path.dirname(__file__))# Ruta relativa # Devuelve primera parte ruta
+# SQLALCHEMY_DATABASE_URI -> Cadena de conexión db. Crear db, pasar directorio y el nombre
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(directorio_base, 'app.sqlite')
+db = SQLAlchemy(app) # Crear objeto db.
+ma = Marshmallow(app)# Trabajar con Marshmallow
+# Esquema de las guías
+class Guía(db.Model): #(Hereda modelo)
+    id = db.Column(db.Integer, primary_key=True) # API cree un nuevo registro en la db-> db +1.
+    título = db.Column(db.String(100), unique=False) # string(Número) Cantidad de caracteres
+    contenido = db.Column(db.String(144), unique=False)
+
+    def __init__(self, título, contenido): # Constructor, método init
+        self.título = título
+        self.contenido = contenido
+        
+# Schema
+class GuíaEsquema(ma.Schema):
+    class Meta:
+        campos = ('título', 'contenido')
+        
+guide_schema = GuíaEsquema() # Crear instancia trabajando con una guía
+guides_schema = GuíaEsquema(many=True) # Crear instancia trabajando con varias guías
 
 if __name__== '__main__': # Condición si el nombre es igual al principal
-    app.run(debug=True) # Ejecutar aplicación 
+    app.run(debug=True) # Ejecutar aplicación
+    
 
