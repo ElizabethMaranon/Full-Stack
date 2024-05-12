146dc9e26ca004c027b6332cf389769a6d4bc6c3
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-05-12 12:32:34 +0200

    Mielma

--- Python/Api/Hola_Flask/app.py
+++ Python/Api/Hola_Flask/app.py
@@ -1,7 +1,9 @@
+from dataclasses import fields
 from flask import Flask # Agregamos las dependencias instaladas, Diferencia mayúsculas de minúsculas
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os # Librería del sistema operativo
+from flask import Flask, request, jsonify # Agregamos librería
 
 app = Flask(__name__) # Creamos variable, en este caso la más básica
 
@@ -10,6 +12,7 @@ directorio_base = os.path.abspath(os.path.dirname(__file__))# Ruta relativa # De
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(directorio_base, 'app.sqlite')
 db = SQLAlchemy(app) # Crear objeto db.
 ma = Marshmallow(app)# Trabajar con Marshmallow
+
 # Esquema de las guías
 class Guía(db.Model): #(Hereda modelo)
     id = db.Column(db.Integer, primary_key=True) # API cree un nuevo registro en la db-> db +1.
@@ -23,10 +26,24 @@ class Guía(db.Model): #(Hereda modelo)
 # Schema
 class GuíaEsquema(ma.Schema):
     class Meta:
-        campos = ('título', 'contenido')
+        fields = ('título', 'contenido')
         
-guide_schema = GuíaEsquema() # Crear instancia trabajando con una guía
-guides_schema = GuíaEsquema(many=True) # Crear instancia trabajando con varias guías
+Guía_Esquema = GuíaEsquema() # Crear instancia trabajando con una guía
+Guías_Esquema = GuíaEsquema(many=True) # Crear instancia trabajando con varias guías
+
+@app.route('/guía', methods=["POST"]) # Primer punto final
+def añadir_guía(): # Crear función añadir guía
+    título = request.json['título'] # Almacenar en variable el valor del título
+    contenido = request.json['contenido'] # Almacenar en variable el valor del contenido
+    
+    nueva_guía = Guía(título, contenido) # Crear instancia de nueva guía
+    
+    db.session.add(nueva_guía) # Comunicarse con db
+    db.session.commit() # Confirmar
+    
+    guía = Guía.query # Realizar consulta
+    
+    return Guía_Esquema.jsonify(guía)
 
 if __name__== '__main__': # Condición si el nombre es igual al principal
     app.run(debug=True) # Ejecutar aplicación
