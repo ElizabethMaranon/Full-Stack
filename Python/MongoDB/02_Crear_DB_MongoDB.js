f33a18f01d62ae25179fb376f941daef4572b6c8
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-05-21 16:06:12 +0200

    Mielma

---
+++ Python/MongoDB/02_Crear_DB_MongoDB.js
@@ -0,0 +1,28 @@
+db.createUser({ // Crear función y argumentos en pares clave-valor
+        user: 'Eli', // Argumento Usuario 
+        pwd: 'Contraseña', // Argumento Contraseña
+        customData: { startDate: new Date() }, // Argumento fecha de inicio
+        roles: [ // Añadir roles 
+          // ClusterAdmin: Mayor acceso gestión cluster
+          //db: 'admin': Usuario admin tiene la capacidad de funcionar como administrador 
+          //readAnyDatabase: Leer y modificar colecciones non-system
+          { role: 'clusterAdmin', db: 'admin' },
+          { role: 'readAnyDatabase', db: 'admin' }, 
+          'readWrite'
+        ]
+      })
+      
+db.getUsers() // Ver usuarios base de datos
+db.dropUser('jon') // Eliminar usuario
+
+db.createUser({
+  user: 'Mielma', 
+  pwd: 'Contraseña',
+  customData: { startDate: new Date() },
+  roles: [
+    { role: 'clusterAdmin', db: 'admin' },
+    { role: 'readAnyDatabase', db: 'admin' }, 
+    'readWrite'
+  ]
+})
+      
