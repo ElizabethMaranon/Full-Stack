89c636433875e24644aa340bebbfdc98afdc624b
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-05-21 22:00:33 +0200

    Mielma

---
+++ Python/MongoDB/05_Insertar_Muchos_Doc_en_Coleccion_MongoDB.js
@@ -0,0 +1,25 @@
+
+
+db.música.insertMany([
+    {
+        "nombre": "Saurom",
+        "publishedDate": new Date(),
+        "canciones": [
+            {"nombre": "Pájaro fantasma"}
+        ]
+    },
+    {
+        "nombre": "Sylvania",
+        "publishedDate": new Date(),
+        "canciones": [
+            {"nombre": "Purgatorium"}
+        ]
+    },
+    {
+        "nombre": "Lepoka",
+        "publishedDate": new Date(),
+        "Canciones": [
+            {"nombre": "Un año más"}
+        ]
+    }
+])
