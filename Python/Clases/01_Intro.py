0f0d99511ba7903e0a0ff373bda6bdae8ead666c
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-05-23 11:36:49 +0200

    Mielma

--- Python/Clases/01_Intro.py
+++ Python/Clases/01_Intro.py
@@ -1,21 +1,21 @@
-"""Un método de instancia es un método que modifica o accede al estado del objeto al que hace referencia. Recibe self como primer parámetro, el cual se convierte en el propio objeto sobre el que estamos trabajando. Python envía este argumento de forma transparente: no hay que pasarlo como argumento."""
-class NombreClase:
-    def funcion(self):# obligaroio yo(self), se explicará mas adelante
-        return('Hola')
+class saludo: # Crear clase
+    def presentación(self):# Crear presentación, obligatorio yo(self)
+        return('Hola, soy Mielma') # Crear retorno
     
-instancia = NombreClase()
+instancia = saludo() # Creamos variable
 
-print(instancia.funcion())
+print(instancia)
+"""imprime <__main__.saludo object at 0x000002093AC77500>
 
-"""imprime <__main__.NombreClase object at 0x0000016B613E5DF0>
 __ se llama dumber, abreviatura de doble subrayado
-factura object -> hemos creado una instancia de NombreClase
-at 0x0000016B613E5DF0 -> Lugar de memoria de la computadora donde Python almacena esta factura
-para que imprima, tenemos que llamar a la función"""
 
-instancia2 = NombreClase()
+factura object -> hemos creado una instancia de presentación
 
-print(instancia2.funcion())
+at 0x0000016B613E5DF0 -> Lugar de memoria de la computadora donde Python almacena esta factura"""
+
+print(instancia.presentación())
+
+
 
 "Ejercicio"
 class Garage:
